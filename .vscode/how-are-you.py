64866d7c4cda9540f0ffe3823ee94a3fa9d993c1
Benjamin Montenegr <benjax.rodeo@gmail.com>
2026-02-12 19:20:29 -0300

    Personalized chat logic, complete more feelings later

---
+++ .vscode/how-are-you.py
@@ -0,0 +1,20 @@
+#Simple?, yes, But is a big step for me :), if u have any notes, please let me know in my X (Twitter)
+
+name = input ("What is your name? ")
+
+question = input ("How are you " + name + "?: ")
+good = ["good", "fine", "perfect", "Good", "GOOD", "Perfect", "Fine", "Really good", "really good"]
+bad = ["bad", "terrible", "Bad", "BAD", "Terrible", "Horrible", "horrible", "Not good"]
+
+if question in good:
+ print ("keep that all day " + name)
+
+
+elif question in bad:
+ print ("go out and u will feel better " + name)
+
+else :
+ print ("I dont know that felling, but thanks for telling me " + name)
+
+
+#Im starting to like coding 
