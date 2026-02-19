fc50d00632e6f94bbeb6bb03e17f35dcc4d6fb1f
Benjamin Montenegr <benjax.rodeo@gmail.com>
2026-02-18 23:31:52 -0300

    my first dollar to arg peso converter

---
+++ dollar-to-ars.py
@@ -0,0 +1,18 @@
+dollar = [{"name": "dolar oficial", "cost": 1370},
+           {"name": "dolar blue", "cost": 1415},
+           {"name": "dolar tarjeta", "cost": 1846},
+           {"name":"dolar mep", "cost": 1408 }]
+
+
+user_request = input("Que dolar desea consultar? ").strip().lower()
+
+
+found = False
+for d in dollar :
+    if user_request == d["name"]: 
+        print (f"{d['name']} cuesta {d['cost']}")
+        found = True
+        break
+
+
+if not found: print("Dolar no encontrado")
